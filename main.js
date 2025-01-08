6cdf44a3a5393a093b80ea7c2113a2f24982dd33
Astronaut <scott@scott-nguyen.com>
2025-01-07 23:18:45 -0600

    Implement getComputerChoice function

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Hello, from main.js")
+
+let humanScore = 0;
+let computerScore = 0;
+
+const option = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    return option[Math.floor(Math.random() * option.length)];
+}
+
+function getHumanChoice() {
+    prompt("Please select your choice: ")
+}
