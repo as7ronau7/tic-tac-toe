109752ff25da9e9dffa8f3496619121a5909168d
Scott Nguyen <101675469+as7ronau7@users.noreply.github.com>
2025-01-22 21:50:02 -0600

    Complete working function of tic tac toe game

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
 console.log("Hello, from main.js")
 
-let humanScore = 0;
-let computerScore = 0;
-
-const option = ['rock', 'paper', 'scissors'];
-
 function getComputerChoice() {
-    return option[Math.floor(Math.random() * option.length)];
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * 3)];
 }
 
 function getHumanChoice() {
-    prompt("Please select your choice: ")
+    return prompt("Select your option: Rock, Paper, Scissors: ");
 }
+
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice())
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoicePick = humanChoice.toLowerCase();
+        computerChoicePick = computerChoice.toLowerCase();
+
+        if (humanChoicePick === computerChoicePick) {
+            console.log('Tie!');
+        } else if (humanChoicePick === 'rock' && computerChoicePick === 'scissors' || humanChoicePick === 'paper' && computerChoicePick === 'rock' || humanChoicePick === 'scissors' && computerChoicePick === 'paper') {
+            humanScore += 1;
+            console.log(`You win! ${humanChoicePick} beats ${computerChoicePick}`);
+        } else {
+            computerScore += 1;
+            console.log(`You lose! ${computerChoicePick} beats ${humanChoicePick}`);
+        }
+    }
+
+    if (humanScore > computerScore) {
+        return `You win! ${humanScore} to ${computerScore}`;
+    } else if (computerScore > humanScore) {
+        return `You lost! ${computerScore} to ${humanScore}`
+    } else {
+        return 'Tie!'
+    }
+    
+}
+playGame()
